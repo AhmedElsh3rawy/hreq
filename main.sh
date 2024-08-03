7c5039bccad6b2d01030f51088babd6960bd927e
Ahmed <ahmedelsharawy117@gmail.com>
2024-08-03 15:33:13 +0300

    feat: handle get request if json or text

--- main.sh
+++ main.sh
@@ -2,11 +2,42 @@
 
 url=$1
 
+headers=$(curl -sSI "$url")
+
+content_type=$(echo "$headers" | grep 'Content-Type:' | awk '{print $2}')
+
 body=$2
 
 reqs=("GET" "POST" "PUT" "PATCH" "DELETE")
 
 req=$(printf "%s\n" "${reqs[@]}" | fzf)
 
-echo "${req} ${url}"
-echo "$body" | jq
+res=""
+
+case $req in
+GET)
+	res=$(curl -sS "$url")
+	;;
+
+POST)
+	echo "POST"
+	;;
+PUT)
+	echo "PUT"
+	;;
+PATCH)
+	echo "PATCH"
+	;;
+DELETE)
+	echo "DELETE"
+	;;
+*)
+	echo "Unknown method: $method"
+	;;
+esac
+
+if [[ "$content_type" == *"application/json"* ]]; then
+	echo "${res}" | jq
+else
+	echo "${res}"
+fi
